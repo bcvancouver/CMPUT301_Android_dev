ddb3a93d62c0716cad3243d61a77c7a52c8dc9f6
bcvancouver <michael-xi@hotmail.com>
2015-10-02 17:36:38 -0600

    dialogue in single user mode works

--- ReactionTimer/app/src/main/java/com/example/michaelxi/reactiontimer/single.java
+++ ReactionTimer/app/src/main/java/com/example/michaelxi/reactiontimer/single.java
@@ -9,27 +9,30 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 
 
 public class Single extends Activity {
 
     long startTime=0;
+    long millisec=0;
 
     //fetched from http://stackoverflow.com/questions/4597690/android-timer-how
     Handler timerHandler= new Handler();
     Runnable timerRunnable= new Runnable() {
-
         @Override
         public void run() {
             Button Click=(Button)findViewById(R.id.click);
-            long millisec=System.currentTimeMillis()-startTime;
-            timerHandler.postDelayed(this, 2000);
+            millisec=System.currentTimeMillis()-startTime;
             Click.setBackgroundColor(Color.GREEN);
-        }
+        };
     };
 
     @Override
@@ -37,10 +40,25 @@ public class Single extends Activity {
         super.onCreate(savedInstanceState);
         //Get the view from single.xml
         setContentView(R.layout.single);
-
         startTime=System.currentTimeMillis();
         timerHandler.postDelayed(timerRunnable, 2000);
-
+        Button Click=(Button)findViewById(R.id.click);
+        Click.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View arg0) {
+                if (millisec<2000) {
+                    AlertDialog SinglePrompt = new AlertDialog.Builder(Single.this).create();
+                    SinglePrompt.setTitle("Warning");
+                    SinglePrompt.setMessage("Don't Click too early");
+                    SinglePrompt.setButton(AlertDialog.BUTTON_NEUTRAL, "Continue", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }
+                    );
+                    SinglePrompt.show();
+                }
+            }
+        });
     }
 
 }
