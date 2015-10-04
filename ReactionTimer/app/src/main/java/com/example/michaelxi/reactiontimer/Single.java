f48b1cff6f7dcde21d225f8a00571385897bbaf6
bcvancouver <michael-xi@hotmail.com>
2015-10-04 16:55:41 -0600

    multi mode works

--- ReactionTimer/app/src/main/java/com/example/michaelxi/reactiontimer/Single.java
+++ ReactionTimer/app/src/main/java/com/example/michaelxi/reactiontimer/Single.java
@@ -152,7 +152,4 @@ public class Single extends Activity {
             }
         });
     }
-
-
-
 }
--- ReactionTimer/app/src/main/java/com/example/michaelxi/reactiontimer/TwoPlayer.java
+++ ReactionTimer/app/src/main/java/com/example/michaelxi/reactiontimer/TwoPlayer.java
@@ -1,15 +1,78 @@
 package com.example.michaelxi.reactiontimer;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by michaelximac on 2015-10-04.
  */
 public class TwoPlayer extends Activity{
+    boolean proceed=true;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         //Get the view from twoplayer.xml
         setContentView(R.layout.twoplayer);
+
+        Button player1=(Button) findViewById(R.id.playerOne);
+        Button player2=(Button) findViewById(R.id.playerTwo);
+
+        player1.setOnClickListener(new Button.OnClickListener() {
+            // Navigating to another activity.
+            //Source code was fetched and modified from http://stackoverflow.com/questions/26097513/android-simple-alert-dialog
+            public void onClick(View arg0) {
+                if (proceed == true) {
+                    proceed=false;
+                    AlertDialog SinglePrompt = new AlertDialog.Builder(TwoPlayer.this).create();
+                    SinglePrompt.setTitle("Result");
+                    SinglePrompt.setMessage("Player 1 Buzz!");
+                    SinglePrompt.setButton(AlertDialog.BUTTON_NEUTRAL, "Restart",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = getIntent();
+                                    finish();
+                                    startActivity(intent);
+                                    proceed=true;
+                                    dialog.dismiss();
+                                }
+                            }
+                    );
+                    SinglePrompt.show();
+                }
+                }
+
+        });
+
+        player2.setOnClickListener(new Button.OnClickListener() {
+            // Navigating to another activity.
+            //Source code was fetched and modified from http://stackoverflow.com/questions/26097513/android-simple-alert-dialog
+            public void onClick(View arg0) {
+                if (proceed==true){
+                    proceed=false;
+                    AlertDialog SinglePrompt = new AlertDialog.Builder(TwoPlayer.this).create();
+                    SinglePrompt.setTitle("Result");
+                    SinglePrompt.setMessage("Player 2 Buzz!");
+                    SinglePrompt.setButton(AlertDialog.BUTTON_NEUTRAL, "Restart",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = getIntent();
+                                    finish();
+                                    startActivity(intent);
+                                    proceed=true;
+                                    dialog.dismiss();
+                                }
+                            }
+                    );
+                    SinglePrompt.show();
+
+                }
+
+            }
+        });
     }
 }
